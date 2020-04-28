574af4b197909263109c2e506c8919f48bde81e0
Dombovari Marton <dombovari.marton@gmail.com>
2020-04-28 08:17:30 +0200

    elakadtam

---
+++ zene/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace zene
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Reader read = new Reader();
+            read.ReadList("musor.txt");
+
+
+
+
+
+        }
+            
+    }
+
+
+}
+
+
---
+++ zene/Track.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zene
+{
+    class Track
+    {
+        private int _radio;
+        private int _time;
+        private string _name;
+
+        public int Radio
+        {
+            get
+            {
+                return this._radio;
+            }
+            set
+            {
+                this._radio = value;
+            }
+        }
+
+        public int Time
+        {
+            get
+            {
+                return this._time;
+            }
+            set
+            {
+                this._time = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+
+        public Track(int radio, int time, string name)
+        {
+            this._radio = radio;
+            this._time = time;
+            this._name = name;
+
+
+            /*
+            string[] data = new string[4];
+            char[] split = { ' ' };
+            data = trackLine.Split(split, 4);
+           
+            this._radio = data[0];
+            this._time = data[1];
+            this._name = data[2];
+            */
+        }
+
+
+
+
+    }
+}
